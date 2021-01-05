d691d11b3033d8e6684e49f1fc3ed5910143e151
Rômulo Rafael <romulo.rafael@take.net>
2021-01-04 23:47:22 -0300

    Create with_Router_Context_Enabled.js
    This script identifies each channel (eg Blip Chat, WhatsApp, Messenger, Telegram, etc.) the user / client is talking to his Smart Contact (Chatbot).

---
+++ Channels/User_Channel_Identification/with_Router_Context_Enabled.js
@@ -0,0 +1,40 @@
+/* [ ! ] Attention¹: This script identifies each channel (eg Blip Chat, WhatsApp, Messenger, Telegram, etc.) 
+the user / client is talking to his Smart Contact (Chatbot). For more information, read the documentation 
+suggested in the comments of this script below. /*
+
+/* [ ! ] Attention²: To configure this script, the following steps are required:
+(0) activate the "Use Router Context" functionality,
+(1) add an "Execute script" action,
+(2) add the "contact.identity" system variable as a input,
+(3) add the code in "Script" and
+(4) save the processing / return in the "channel" variable (or equivalent) */
+
+/* [ ! ] API Reference: https://docs.blip.ai/#channels */
+
+function run(contactIdentity) {
+    contactDomain = contactIdentity.split("@")[1];
+    let channel;
+    switch (contactDomain) {
+        case 'wa.gw.msging.net':
+            channel = "Whatsapp";
+        case '0mn.io':
+            channel = "Blip Chat";
+        case 'messenger.gw.msging.net':
+            channel = "Messenger";
+        case 'businessmessages.gw.msging.net':
+            channel = "GBM";
+        case 'skype.gw.msging.net':
+            channel = "Skype";
+        case 'telegram.gw.msging.net':
+            channel = "Telegram";
+        case 'workplace.gw.msging.net':
+            channel = 'Workplace';
+        case 'mailgun.gw.msging.net':
+            channel = "Email";
+        case 'take.io':
+            channel = "SMS";         
+        default:
+            channel = "Other";
+    }
+    return channel;
+}
