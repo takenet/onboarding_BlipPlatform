672c1d4d0b2facc29ba314a3e173f4fc9fc8394f
kathleenmorita <kathleen.lima@take.net>
2021-05-28 11:56:04 -0300

    Update errorCounter.js

--- Expections/errorCounter.js
+++ Expections/errorCounter.js
@@ -1,22 +1,22 @@
 /* # Technical Note # 
 
 This script was design to count how many errors the user got in a single container, to start an exceptions/error loop and, 
-after a defined number of errors, the user will be directed to a specific container (ie. operator).
+after a defined number of errors, the user will be directed to a specific block (ie. operator).
 
 */
 
 /* # Prerequisites #
 
 The return variable needs to be defined at the beginning of the chatbot flow with the value "0".
-Unique error container for each container
+Unique error block for each container
 
 /*
 
 /* # How to implement #
 
-Create an error container
-Connect the error container with the original container you want to count the return errors
-Add the script below under 'Entering Actions' > 'Execute script' in the error container
+Create an error block
+Connect the error block with the original block you want to count the return errors
+Add the script below under 'Entering Actions' > 'Execute script' in the error block
 Define a return variable under 'Save return' > 'Return value variable'
 Update the value to this variable to "0" at the beginning of the chatbot flow.
 
