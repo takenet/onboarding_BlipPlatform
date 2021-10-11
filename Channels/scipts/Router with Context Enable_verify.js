c561ac757f9fca294c91fde42a50b0288bbc3371
Rômulo Rafael <romulo.rafael@take.net>
2021-10-11 10:55:14 -0300

    Update script

--- Channels/scipts/Router with Context Enable_verify.js
+++ Channels/scipts/Router with Context Enable_verify.js
@@ -2,11 +2,10 @@
 
 /* [ ! ] Attention¹: This script identifies each channel (eg Blip Chat, WhatsApp, Messenger, Telegram, etc.) 
 the user / client is talking to his Smart Contact (Chatbot).
-
 /* [ ! ] Attention²: To configure this script, the following steps are required:
 (0) activate the "Use Router Context" functionality,
 (1) add an "Execute script" action,
-(2) add the "contact.identity" system variable as a input,
+(2) add the "tunnel.originator" or "contact.identity system variable as a input,
 (3) add the code in "Script" and
 (4) save the processing / return in the "channel" variable (or equivalent) */
 
@@ -18,33 +17,55 @@ function run(contactIdentity) {
     contactDomain = contactIdentity.split("@")[1];
 
     switch (contactDomain) {
-        case 'wa.gw.msging.net':
-            channel = "Whatsapp";
-            return channel;
+        
         case '0mn.io':
             channel = "Blip Chat";
             return channel;
+
+        case 'tangram.com.br':
+            channel = "SMS";
+            return channel;
+
+         case 'take.io':
+            channel = "SMS";
+            return channel;
+
         case 'messenger.gw.msging.net':
             channel = "Messenger";
             return channel;
+        
+        case 'instagram.gw.msging.net':
+            channel = "Instagram";
+            return channel;
+
+        case 'wa.gw.msging.net':
+            channel = "Whatsapp";
+            return channel;
+
+        case 'abs.gw.msging.net':
+            channel = "Microsoft Teams";
+            return channel;
+
         case 'businessmessages.gw.msging.net':
             channel = "GBM";
             return channel;
+
         case 'skype.gw.msging.net':
             channel = "Skype";
             return channel;
+
         case 'telegram.gw.msging.net':
             channel = "Telegram";
             return channel;
+
         case 'workplace.gw.msging.net':
             channel = 'Workplace';
             return channel;
+
         case 'mailgun.gw.msging.net':
             channel = "Email";
-            return channel;
-        case 'take.io':
-            channel = "SMS";
-            return channel;
+            return channel;   
+
         default:
             channel = "Other";
             return channel;
