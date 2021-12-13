4981ba1604c9187d3b5c09061b18602a97b25443
Rômulo Rafael <romulo.rafael@take.net>
2021-12-13 11:47:08 -0300

    Update Router with Context Disable_verify.js

--- Channels/scripts/Router with Context Disable_verify.js
+++ Channels/scripts/Router with Context Disable_verify.js
@@ -13,41 +13,64 @@ suggested in the comments of this script below. /*
 
 /* [ ! ] API Reference: https://docs.blip.ai/#channels */
 
-function run(tunnelOriginator) {
+function run(contactIdentity) { 
     var channel;
     
-    contactDomain = tunnelOriginator.split("@")[1];
-    
+    contactDomain = contactIdentity.split("@")[1];
+
     switch (contactDomain) {
-        case 'wa.gw.msging.net':
-            channel = "Whatsapp";
-            return channel;
+        
         case '0mn.io':
             channel = "Blip Chat";
             return channel;
+
+        case 'tangram.com.br':
+            channel = "SMS";
+            return channel;
+
+         case 'take.io':
+            channel = "SMS";
+            return channel;
+
         case 'messenger.gw.msging.net':
             channel = "Messenger";
             return channel;
+        
+        case 'instagram.gw.msging.net':
+            channel = "Instagram";
+            return channel;
+
+        case 'wa.gw.msging.net':
+            channel = "WhatsApp";
+            return channel;
+
+        case 'abs.gw.msging.net':
+            channel = "Microsoft Teams";
+            return channel;
+
         case 'businessmessages.gw.msging.net':
             channel = "GBM";
             return channel;
+
         case 'skype.gw.msging.net':
             channel = "Skype";
             return channel;
+
         case 'telegram.gw.msging.net':
             channel = "Telegram";
             return channel;
+
         case 'workplace.gw.msging.net':
             channel = 'Workplace';
             return channel;
+
         case 'mailgun.gw.msging.net':
             channel = "Email";
-            return channel;
-        case 'take.io':
-            channel = "SMS";
-            return channel;
+            return channel;   
+
         default:
             channel = "Other";
             return channel;
     }
+}
 }
