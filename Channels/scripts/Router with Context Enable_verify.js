82ffc5a8c54bceda4ba2e3215604147f2cdccc2d
Rômulo Rafael <romulo.rafael@take.net>
2021-12-13 11:45:29 -0300

    Update Router with Context Enable_verify.js

--- Channels/scripts/Router with Context Enable_verify.js
+++ Channels/scripts/Router with Context Enable_verify.js
@@ -18,33 +18,55 @@ function run(contactIdentity) {
     contactDomain = contactIdentity.split("@")[1];
 
     switch (contactDomain) {
-        case 'wa.gw.msging.net':
-            channel = "Whatsapp";
-            return channel;
+        
         case '0mn.io':
             channel = "Blip Chat";
             return channel;
+
+        case 'tangram.com.br':
+            channel = "SMS";
+            return channel;
+
+         case 'take.io':
+            channel = "SMS";
+            return channel;
+
         case 'messenger.gw.msging.net':
             channel = "Messenger";
             return channel;
+        
+        case 'instagram.gw.msging.net':
+            channel = "Instagram";
+            return channel;
+
+        case 'wa.gw.msging.net':
+            channel = "WhatsApp";
+            return channel;
+
+        case 'abs.gw.msging.net':
+            channel = "Microsoft Teams";
+            return channel;
+
         case 'businessmessages.gw.msging.net':
             channel = "GBM";
             return channel;
+
         case 'skype.gw.msging.net':
             channel = "Skype";
             return channel;
+
         case 'telegram.gw.msging.net':
             channel = "Telegram";
             return channel;
+
         case 'workplace.gw.msging.net':
             channel = 'Workplace';
             return channel;
+
         case 'mailgun.gw.msging.net':
             channel = "Email";
-            return channel;
-        case 'take.io':
-            channel = "SMS";
-            return channel;
+            return channel;   
+
         default:
             channel = "Other";
             return channel;
